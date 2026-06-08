b0f3da573c36274cfd90fa17079e7085c70ea833
Aniket Singh <27aniketsingh077@gmail.com>
2026-06-08 12:55:42 +0530

    Created: Admin route

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,3 +1,23 @@
 import jwt from "jsonwebtoken"
 import User from "../models/User"
 
+const protect = async (req, res, next) => {
+        let token;
+        if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+                try{
+                        token = req.headers.authorization.split('')[1];
+                        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+                        req.user = await User.findBy(decoded.id).select('-password');
+                        next()
+                }
+                catch(error){
+                        return  res.status(401).json({message: "Unauthorized, Token not matched"})
+                }
+        }
+
+        if(!token) {
+                return res.status(500).json({ message: "Not authorized, No token"})
+        }
+}
+
+export default protect;
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import {registerUser, loginUser, getUsers} from "../controllers/authController";
+import {protect} from "../middleware/authMiddleware";
+import {admin} from "../middleware/adminMiddleware"
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/users", protect, admin, getUsers)
+router.get("/users", protect, admin, getUsers);
 
 export default router;
